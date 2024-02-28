03a254338f9870054b75c7123eb3859fd19d2599
Zakaria Hafeez <21221@students.mrgs.school.nz>
2024-02-28 14:47:13 +1300

    initial commit

--- GUI/test.py
+++
@@ -1,9 +0,0 @@
-def reversed_string(input_string):
-    reversed_string = ""
-    for char in input_string:
-        reversed_string = char + reversed_string
-    return reversed_string
-
-
-print(reversed_string("Zak"))
-print(reversed_string("Leeeeeeeel"))
